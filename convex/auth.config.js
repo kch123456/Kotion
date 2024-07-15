abfb53626e95a7193adf1573c0a772ebbe1f0093
Kevin Chen <Kunhongchen@umass.edu>
2024-07-14 22:00:24 -0400

    add image

--- convex/auth.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  providers:[
-    {
-      domain:'https://equal-moth-69.clerk.accounts.dev',
-      applicationID:"convex", 
-    }
-  ]
-}
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://peaceful-glider-74.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
